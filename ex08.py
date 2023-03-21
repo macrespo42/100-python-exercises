16d6ca6211cd0720dbe6a1ae3425bb657a4c8945
macrespo42 <macrespo@student.42.fr>
2023-03-21 15:49:23 +0100

    feat: ex08

---
+++ ex08.py
@@ -0,0 +1,10 @@
+index = 10
+print('with while :')
+while (index <= 20):
+    if (index % 2 != 0):
+        print(index)
+    index += 1
+print('with for :')
+for x in range(10, 21):
+    if (x % 2 != 0):
+        print(x)
