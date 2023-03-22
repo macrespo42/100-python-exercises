a35df8dfac856c58865ec4cbd08971c15685b5ec
macrespo42 <macrespo@student.42.fr>
2023-03-22 13:31:04 +0100

    feat: ex17 use of lambda

--- beginner/ex17.py
+++ beginner/ex17.py
@@ -1,6 +1,3 @@
-def getNumberOfFruits(fruits):
-   return fruits[1] 
-
 l = [("Pomme", 15), ("Banane", 8), ("Fraise", 12), ("Kiwi", 9), ("Peche", 2)]
-l.sort(key=getNumberOfFruits)
+l.sort(key= lambda x: x[1])
 print(l)
